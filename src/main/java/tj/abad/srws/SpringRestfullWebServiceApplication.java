0e1a64a7fc59c52eccbf3f6143e5be2c7eb80097
Dilshod Mizrobov <dilshod.mizrobov1994@gmail.com>
2021-04-26 13:08:02 +0500

    Initial commit

---
+++ src/main/java/tj/abad/srws/SpringRestfullWebServiceApplication.java
@@ -0,0 +1,13 @@
+package tj.abad.srws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfullWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestfullWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tj/abad/srws/SpringRestfullWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tj.abad.srws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfullWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
