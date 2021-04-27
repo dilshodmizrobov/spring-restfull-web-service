20951ca95193844a81d981879d7fb1fc77927c15
Dilshod Mizrobov <dilshod.mizrobov1994@gmail.com>
2021-04-27 23:24:14 +0500

    Add security to app

--- src/main/java/tj/abad/srws/SpringRestfullWebServiceApplication.java
+++ src/main/java/tj/abad/srws/SpringRestfullWebServiceApplication.java
@@ -2,6 +2,11 @@ package tj.abad.srws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tj.abad.srws.config.AppProperties;
+import tj.abad.srws.config.SpringApplicationContext;
 
 @SpringBootApplication
 public class SpringRestfullWebServiceApplication {
@@ -10,4 +15,20 @@ public class SpringRestfullWebServiceApplication {
         SpringApplication.run(SpringRestfullWebServiceApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringApplicationContext springApplicationContext(){
+        return new SpringApplicationContext();
+    }
+
+    @Bean(name="AppProperties")
+    public AppProperties getAppProperties()
+    {
+        return new AppProperties();
+    }
 }
+
--- src/main/java/tj/abad/srws/service/impl/UserServiceImpl.java
+++ src/main/java/tj/abad/srws/service/impl/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package tj.abad.srws.service.impl;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import tj.abad.srws.io.entity.UserEntity;
 import tj.abad.srws.io.repository.UserRepository;
@@ -8,7 +12,7 @@ import tj.abad.srws.service.UserService;
 import tj.abad.srws.shared.Utils;
 import tj.abad.srws.shared.dto.UserDto;
 
-import java.util.UUID;
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,18 +21,21 @@ public class UserServiceImpl implements UserService {
 
     private final Utils utils;
 
-    public UserServiceImpl(UserRepository userRepository, Utils utils) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, Utils utils, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.utils = utils;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDto create(UserDto newUser) {
-        if(userRepository.findUserByEmail(newUser.getEmail()).isPresent()) throw new RuntimeException("User exist");
+        if(userRepository.findByEmail(newUser.getEmail()).isPresent()) throw new RuntimeException("User exist");
 
         var userEntity = new UserEntity();
         BeanUtils.copyProperties(newUser, userEntity);
-        userEntity.setEncryptedPassword("test");
+        userEntity.setEncryptedPassword(passwordEncoder.encode(newUser.getPassword()));
         userEntity.setUserId(utils.generateUserId(30));
 
         var storedUserDetails = userRepository.save(userEntity);
@@ -38,5 +45,19 @@ public class UserServiceImpl implements UserService {
 
         return userDto;
 
+    }
+
+    @Override
+    public UserDto getUser(String email) {
+        var userEntity=userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not fount"));
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(userEntity, returnValue);
+        return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) {
+        var user=userRepository.findByEmail(userName).orElseThrow(() -> new UsernameNotFoundException("User not fount"));
+        return new User(user.getEmail(), user.getEncryptedPassword(), new ArrayList<>());
     }
 }
