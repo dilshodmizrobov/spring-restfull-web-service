a8affe1c31693908454182800510b28491aee7a0
Dilshod Mizrobov <dilshod.mizrobov1994@gmail.com>
2021-04-28 18:45:54 +0500

    create war file

--- src/main/java/tj/abad/srws/SpringRestfullWebServiceApplication.java
+++ src/main/java/tj/abad/srws/SpringRestfullWebServiceApplication.java
@@ -2,6 +2,8 @@ package tj.abad.srws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,7 +11,11 @@ import tj.abad.srws.config.AppProperties;
 import tj.abad.srws.config.SpringApplicationContext;
 
 @SpringBootApplication
-public class SpringRestfullWebServiceApplication {
+public class SpringRestfullWebServiceApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestfullWebServiceApplication.class, args);
