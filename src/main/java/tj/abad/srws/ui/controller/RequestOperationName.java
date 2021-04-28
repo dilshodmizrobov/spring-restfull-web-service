b8487da2dba08e845175ebd4a06aef1b3210de93
Dilshod Mizrobov <dilshod.mizrobov1994@gmail.com>
2021-04-28 10:44:51 +0500

    Add crud operations

---
+++ src/main/java/tj/abad/srws/ui/controller/RequestOperationName.java
@@ -0,0 +1,8 @@
+package tj.abad.srws.ui.controller;
+
+public enum RequestOperationName {
+	DELETE,
+	VERIFY_EMAIL,
+	REQUEST_PASSWORD_RESET,
+	PASSWORD_RESET
+}
--- src/main/java/tj/abad/srws/ui/controller/UserController.java
+++ src/main/java/tj/abad/srws/ui/controller/UserController.java
@@ -1,37 +1,90 @@
 package tj.abad.srws.ui.controller;
 
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import tj.abad.srws.service.UserService;
 import tj.abad.srws.shared.dto.UserDto;
 import tj.abad.srws.ui.model.request.UserDetailsRequestModel;
+import tj.abad.srws.ui.model.response.OperationStatusModel;
+import tj.abad.srws.ui.model.response.RequestOperationStatus;
 import tj.abad.srws.ui.model.response.UserRest;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users
 //@CrossOrigin(origins= {"http://localhost:8083", "http://localhost:8084"})
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
 
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-	@PostMapping("")
-	public UserRest create(@RequestBody  UserDetailsRequestModel userDetails) {
-		var userRest = new UserRest();
-		var userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails, userDto);
-		var createdUser = userService.create(userDto);
-		BeanUtils.copyProperties(createdUser, userRest);
-		return userRest;
-	}
+    @PostMapping(value = "",
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest create(@RequestBody UserDetailsRequestModel userDetails) {
+        var userRest = new UserRest();
+        var userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+        var createdUser = userService.create(userDto);
+        BeanUtils.copyProperties(createdUser, userRest);
+        return userRest;
+    }
+    @PutMapping(path = "/{id}", consumes = { MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE })
+    public UserRest updateUser(@PathVariable String id, @RequestBody UserDetailsRequestModel userDetails) {
+        UserRest returnValue = new UserRest();
 
-	@GetMapping("")
-	public String getAll() {
-		return "Hello";
-	}
+        UserDto userDto = new UserDto();
+        userDto = new ModelMapper().map(userDetails, UserDto.class);
+
+        UserDto updateUser = userService.updateUser(id, userDto);
+        returnValue = new ModelMapper().map(updateUser, UserRest.class);
+
+        return returnValue;
+    }
+
+    @DeleteMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public OperationStatusModel deleteUser(@PathVariable String id) {
+        OperationStatusModel returnValue = new OperationStatusModel();
+        returnValue.setOperationName(RequestOperationName.DELETE.name());
+
+        userService.deleteUser(id);
+
+        returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+        return returnValue;
+    }
+
+    @GetMapping(produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+                                   @RequestParam(value = "limit", defaultValue = "2") int limit) {
+        List<UserRest> returnValue = new ArrayList<>();
+
+        List<UserDto> users = userService.getUsers(page, limit);
+
+        Type listType = new TypeToken<List<UserRest>>() {
+        }.getType();
+        returnValue = new ModelMapper().map(users, listType);
+
+         return returnValue;
+    }
+
+    @GetMapping(value = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest getUserById(@PathVariable String userId) {
+        var userRest = new UserRest();
+        var userDto = userService.findUserByUserId(userId);
+        BeanUtils.copyProperties(userDto, userRest);
+        return userRest;
+    }
 
 }
